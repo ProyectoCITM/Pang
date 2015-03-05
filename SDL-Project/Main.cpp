3d140215e21b4058a3bc2f19f89c0340d241f0a7
pask1 <ericpascualriu10@gmail.com>
2015-03-05 19:25:54 +0100

    Inici

---
+++ SDL-Project/Main.cpp
@@ -0,0 +1,13 @@
+#include <SDL.h>
+
+#pragma comment( lib, "SDL2-2.0.3/lib/x86/SDL2.lib")
+#pragma comment( lib, "SDL2-2.0.3/lib/x86/SDL2main.lib")
+
+int main(int argc, char** argv)
+{
+
+
+	return 0;
+}
+
+
